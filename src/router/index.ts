5a905fab1b82818c4759a3e5925a47fa35a1e777
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-16 10:14:39 +0100

    Add sections

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHashHistory  } from "vue-router"
-import HomeView from "../views/HomeView.vue"
+import Home from "../views/home/Home.vue"
+import About from "../views/about/About.vue"
+import Projects from "../views/projects/Projects.vue"
+import Resume from "../views/resume/Resume.vue"
+import Contacts from "../views/contacts/Contacts.vue"
 
 const router = createRouter({
     history: createWebHashHistory(),
@@ -7,7 +11,27 @@ const router = createRouter({
         {
             path: "/",
             name: "home",
-            component: HomeView
+            component: Home
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: About
+        },
+        {
+            path: "/projects",
+            name: "projects",
+            component: Projects
+        },
+        {
+            path: "/resume",
+            name: "resume",
+            component: Resume
+        },
+        {
+            path: "/contacts",
+            name: "contacts",
+            component: Contacts
         }
     ]
 })
---
+++ src/views/resume/Resume.vue
@@ -0,0 +1,20 @@
+<template>
+    <Navbar />
+
+    <main>
+        <div class="container mx-auto">
+            <h1 class="text-5xl font-bold">
+                {{ t("hello") }}
+            </h1>
+        </div>
+    </main>
+</template>
+
+
+<script setup lang="ts">
+    import Navbar from "@/components/navbar/Navbar.vue";
+    import { useI18n } from "vue-i18n";
+    import locale from "./locale.json";
+
+    const { t } = useI18n({ messages: locale });
+</script>
