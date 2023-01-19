e807d842b659e5e849ca293b19e161a78cdb7710
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-19 20:14:31 +0100

    Update background color

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white text-gray-900 min-h-screen h-full
+    <div class="bg-gray-100 text-gray-900 min-h-screen h-full
                 dark:bg-gray-900 dark:text-slate-50"
          style="font-family: 'Comfortaa';">
         <div class="container mx-auto pb-8 md:px-8 h-full w-full">
