c1aa7564edd81d790d2b35e268388c6b3dfd3ff8
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-19 13:09:20 +0100

    Fix selection in contacts

--- src/views/contacts/ContactLink.vue
+++ src/views/contacts/ContactLink.vue
@@ -2,7 +2,7 @@
     <p class="my-4 text-center">
         <a :href="props.url">
             <div class="inline-flex items-center">
-                <img :src="props.icon" alt="Github" class="h-7 mr-2 dark:invert" /> 
+                <img :src="props.icon" alt="Github" class="h-7 mr-2 select-none dark:invert" /> 
                 <span class="text-xl truncate">{{ props.label }}</span>
             </div>
         </a>
