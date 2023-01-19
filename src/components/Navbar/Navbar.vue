8c442bc25be0ce77934e6884f5fce4866bf58d40
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-19 19:27:49 +0100

    Update navbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,17 +1,15 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900 px-2 sm:px-4 py-2.5 ">
-    <div class="container flex flex-wrap items-center justify-between md:justify-start mx-auto">
-        <div></div>
-
-        <div class="block md:flex">
-            <div class="flex justify-end items-center order-2">
+    <nav class="bg-white border-gray-200 dark:bg-gray-900 py-2.5">
+    <div class="container flex flex-wrap items-center justify-end md:justify-start mx-auto">
+        <div class="block md:flex w-full">
+            <div class="flex justify-end items-center order-2 md:w-1/2">
                 <a href="https://github.com/NotXia" class="rounded-full p-1 mx-1 hover:bg-slate-200 dark:hover:bg-slate-700">
                     <img :src="github_icon" alt="Github" class="h-5 dark:invert" /> 
                 </a>
                 <ThemeSwitch class="mx-1" />
                 <LanguageSelector class="mx-1" />
 
-                <button class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 md:hidden dark:text-gray-400"
+                <button class="inline-flex items-center mx-3 text-sm text-gray-500 md:hidden dark:text-gray-400"
                     data-collapse-toggle="navbar-main" type="button" aria-controls="navbar-main" aria-expanded="false">
                     <span class="sr-only">{{ t("open nav") }}</span>
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -20,7 +18,7 @@
                 </button>
             </div>
 
-            <div class="flex items-center order-1">
+            <div class="flex items-center order-1 md:w-1/2">
                 <div class="hidden w-full md:block md:w-auto" id="navbar-main">
                     <ul class="flex flex-col py-4 pr-4 mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium bg-transparent">
                         <NavLink to="/" :label="t('home')"/>
