6a880b1c8f48922a2a28c9528d24593c28b51a70
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-24 20:45:36 +0100

    Update page positioning

--- src/views/contacts/Contacts.vue
+++ src/views/contacts/Contacts.vue
@@ -3,18 +3,16 @@
         <Navbar />
     </div>
 
-    <main class="absolute top-0 left-0 h-screen w-screen z-0">
-        <div class="flex justify-center items-center h-full w-full">
-            <div>
-                <h1 class="text-5xl font-bold text-center">{{ t("contacts") }}</h1>
-                
-                <div class="flex justify-center">
-                    <div>
-                        <ContactLink label="tcxia2001@gmail.com" url="mailto:tcxia2001@gmail.com" :icon="mail_icon" alt="E-mail" />
-                        <ContactLink label="t.me/notxia" url="https://t.me/notxia" :icon="telegram_icon" alt="Telegram" />
-                        <ContactLink label="Panda#9784" url="https://discordapp.com/users/272354672969515009" :icon="discord_icon" alt="Discord" />
-                        <ContactLink label="linkedin.com/in/tian-cheng-xia" url="https://www.linkedin.com/in/tian-cheng-xia/" :icon="linkedin_icon" alt="LinkedIn" />
-                    </div>
+    <main class="absolute top-0 flex justify-center items-center left-0 h-full w-full z-0">
+        <div>
+            <h1 class="text-5xl font-bold text-center">{{ t("contacts") }}</h1>
+            
+            <div class="flex justify-center">
+                <div>
+                    <ContactLink label="tcxia2001@gmail.com" url="mailto:tcxia2001@gmail.com" :icon="mail_icon" alt="E-mail" />
+                    <ContactLink label="t.me/notxia" url="https://t.me/notxia" :icon="telegram_icon" alt="Telegram" />
+                    <ContactLink label="Panda#9784" url="https://discordapp.com/users/272354672969515009" :icon="discord_icon" alt="Discord" />
+                    <ContactLink label="linkedin.com/in/tian-cheng-xia" url="https://www.linkedin.com/in/tian-cheng-xia/" :icon="linkedin_icon" alt="LinkedIn" />
                 </div>
             </div>
         </div>
