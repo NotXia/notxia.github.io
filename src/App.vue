a7278796bc75a7557e518772562b5a87d485ff8a
NotXia <35894453+NotXia@users.noreply.github.com>
2023-01-24 21:36:43 +0100

    Add padding on mobile

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="bg-gray-100 text-gray-900 min-h-screen h-full
                 dark:bg-gray-900 dark:text-slate-50"
          style="font-family: 'Comfortaa';">
-        <div class="container mx-auto pb-8 md:px-8 min-h-screen min-w-screen flex flex-col">
+        <div class="container mx-auto pb-8 px-3 md:px-8 min-h-screen min-w-screen flex flex-col">
             <RouterView />
         </div>
     </div>
