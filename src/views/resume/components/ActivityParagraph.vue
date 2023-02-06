1dbebf3d623b0dc2d748f634f27f288fdc5c83b7
NotXia <35894453+NotXia@users.noreply.github.com>
2023-02-06 20:09:17 +0100

    Add skills list

---
+++ src/views/resume/components/ActivityParagraph.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="md:ml-2 mt-4">
+        <div class="flex justify-between">
+            <h3 class="text-xl font-semibold tracking-wide">{{ props.title }}</h3>
+            <div class="text-right text-gray-500 dark:text-gray-400">{{ props.right_text }}</div>
+        </div>
+        <div class="leading-5 text-gray-500 dark:text-gray-400">{{ props.subtitle }}</div>
+
+        <div class="mt-1">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+    const props = defineProps({
+        title: String,
+        subtitle: String,
+        right_text: String
+    });
+</script>
---
+++ src/views/resume/components/ProgrammingLogo.vue
@@ -0,0 +1,19 @@
+<template>
+    
+    <div class="inline-block">
+        <div class="flex items-center">
+            <img :src="props.logo" alt="" :class="`h-5 mr-1 ${props.needInvert ? 'dark:invert' : ''}`">
+            {{ props.language }}
+        </div>
+    </div>
+
+</template>
+
+
+<script setup lang="ts">
+    const props = defineProps({
+        language: String,
+        logo: String,
+        needInvert: Boolean
+    });
+</script>
