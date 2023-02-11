2412cfdceacf9a023c82f328c392ed3ccef535d4
NotXia <35894453+NotXia@users.noreply.github.com>
2023-02-11 19:41:34 +0100

    Add margin for scrollbar shift

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="bg-gray-100 text-gray-900 min-h-screen h-full
                 dark:bg-gray-900 dark:text-slate-50"
-         style="font-family: 'Comfortaa';">
-        <div class="container mx-auto pb-8 px-3 md:px-8 min-h-screen min-w-screen flex flex-col">
-            <RouterView />
+         style="font-family: 'Comfortaa'; overflow-x: hidden; ">
+        
+        <!-- Prevents scrollbar shift -->
+         <div style="margin-right: calc(-1 * (100vw - 100%));">
+            <div class="container mx-auto pb-8 px-3 md:px-8 min-h-screen w-screen flex flex-col">
+                <RouterView />
+            </div>
         </div>
 
         <Cookie />
