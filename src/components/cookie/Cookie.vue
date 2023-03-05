b5f6ed847763936e8b7fb331335ff9c33f025428
NotXia <35894453+NotXia@users.noreply.github.com>
2023-03-05 19:57:45 +0100

    Update cookie banner text

--- src/components/cookie/Cookie.vue
+++ src/components/cookie/Cookie.vue
@@ -33,10 +33,9 @@
         "en": {
             "cookie policy title": "Cookie policy",
             "cookie policy": "This website uses cookies but not for analysis purposes and they are not sent to third parties. " +  
-                             "Actually, the only cookies here are those that I ate for breakfast, " +
-                             "but since no one ever reads these banners, no one will know what I ate this morning. " +
-                             "Maybe your reading too much, but reading is healthy, so I won't stop you. " +
-                             "Thank you for keeping me company, I wish you a great day and if you would like some cookies too do not hesitate to click on the following link:",
+                             "The only cookies here are those that I ate this morning, although I prefer a savory breakfast. " +
+                             "I don't know why you are reading this, but thanks for keeping me company, I wish you a great day. " +
+                             "If you would like some cookies too, do not hesitate to click on the following link:",
             "cookie policy link": "Click here to read the policy",
             "accept": "Accept", 
             "reject": "Reject"
@@ -44,10 +43,9 @@
         "it": {
             "cookie policy title": "Informativa cookie",
             "cookie policy": "Questo sito utilizza cookie ma non per fini di profilazione e non sono inviati a terze parti. " +  
-                             "In realtà gli unici cookie presenti sono quelli che ho mangiato a colazione, " +
-                             "ma dato che nessuno fa caso a questi banner, nessuno saprà cos'ho mangiato questa mattina. " +
-                             "Forse stai leggendo un po' troppo, ma leggere fa bene, quindi non ti fermerò. " +
-                             "Ti ringrazio per avermi fatto compagnia, ti auguro una buona giornata e se gradisci dei biscotti non esitare a cliccare l'informativa al seguente link:",
+                             "Gli unici cookie presenti sono quelli che ho mangiato questa mattina, anche se in realtà preferisco una colazione salata. " +
+                             "Non so perché stai leggendo questo, ma ti ringrazio per avermi fatto compagnia e ti auguro una buona giornata. " + 
+                             "Se gradisci dei biscotti, non esitare a cliccare l'informativa al seguente link:",
             "cookie policy link": "Clicca qui per l'informativa cookie",
             "accept": "Accetta", 
             "reject": "Rifiuta"
