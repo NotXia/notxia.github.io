bf5fe540038f00ac025e263a164ddcae867d7c40
NotXia <35894453+NotXia@users.noreply.github.com>
2023-03-10 18:43:56 +0100

    Add home page

--- src/components/random-something/RandomSomething.vue
+++ src/components/random-something/RandomSomething.vue
@@ -12,21 +12,33 @@
     import { addFoundEasterEgg } from "@/utilities/easteregg_handler";
     import penguin_image from "@/assets/images/penguin.png";
     import llama_image from "@/assets/images/llama.png";
+    import rock_image from "@/assets/images/rock.png";
+    import coconut_image from "@/assets/images/coconut.png";
+    import red_panda_image from "@/assets/images/red-panda.png";
 
     const { t } = useI18n({ messages: {
         "en": {
             "penguin": "This is a penguin",
-            "llama": "This is a llama"
+            "llama": "This is a llama",
+            "rock": "This is a rock",
+            "coconut": "This is a coconut",
+            "red panda": "This is a red panda"
         },
         "it": {
             "penguin": "Questo è un pinguino",
-            "llama": "Questo è un lama"
+            "llama": "Questo è un lama",
+            "rock": "Questo è un sasso",
+            "coconut": "Questa è una noce di cocco",
+            "red panda": "Questo è un panda rosso"
         }
     } });
 
     const things = [
         { name: "penguin", image: penguin_image },
-        { name: "llama", image: llama_image }
+        { name: "llama", image: llama_image },
+        { name: "rock", image: rock_image },
+        { name: "coconut", image: coconut_image },
+        { name: "red panda", image: red_panda_image }
     ];
 
     const current_name = ref("");
