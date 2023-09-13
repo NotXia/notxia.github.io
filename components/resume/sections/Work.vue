a84315cabf170ae668f4caba2de0425cdc2e12ed
NotXia <35894453+NotXia@users.noreply.github.com>
2023-09-13 16:06:13 +0200

    Update work

--- components/resume/sections/Work.vue
+++ components/resume/sections/Work.vue
@@ -5,6 +5,12 @@
         <ActivityParagraph :title="$t('pcto toyota')" subtitle="Toyota Material Handling Manufacturing Italy" 
                            :right_text="`${$t('december')} 2019 | ${$t('july')} 2019 | ${$t('february')} 2019`">
             {{ $t("pcto toyota description") }}
+            <ul class="list-inside list-disc">
+                <li>{{ $t("pcto toyota description list1") }}</li>
+                <li>{{ $t("pcto toyota description list2") }}</li>
+                <li>{{ $t("pcto toyota description list3") }}</li>
+                <li>{{ $t("pcto toyota description list4") }}</li>
+            </ul>
         </ActivityParagraph>
     </div>
 </template>
