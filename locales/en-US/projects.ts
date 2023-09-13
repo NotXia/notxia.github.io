86eea1eaa493a339dc9c6a3b708d7d91f0e56426
NotXia <35894453+NotXia@users.noreply.github.com>
2023-09-13 15:37:05 +0200

    Update projects

--- locales/en-US/projects.ts
+++ locales/en-US/projects.ts
@@ -13,11 +13,11 @@ export default {
 
     
     "bsc_thesis.title": "Bachelor's thesis",
-    "bsc_thesis.description": "Something related to the application of Large Language Models in the biomedical field.",
+    "bsc_thesis.description": "Automatic text summarization of biomedical publications using pretrained language models.",
 
     "wavemap.description": 
         "Project for the Mobile Applications Laboratory course.\n" + 
-        "Android application displaying the strength of signals (Wi-Fi, Bluetooth, LTE) and noise level through a heat-map.",
+        "Android application displaying, using a heat-map, the strength of signals (Wi-Fi, Bluetooth, LTE) and noise level.",
 
     "wirefilter.description":
         "Virtual Distributed Ethernet (VDE) is an open source project that allows the creation of virtual networks through a set of plugins.\n" +
