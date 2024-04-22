2e80c87e56493292fcd98f9eb1cea0803093166c
NotXia <NotXia@users.noreply.github.com>
2024-04-22 18:30:59 +0000

    deploy: 765ac4ac99a353db4b4c383b3427878f3f2aa985

--- _nuxt/JVmmSU3P.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,e as c,f as o,s as n}from"./3yhvywNI.js";const _={},r={class:"flex flex-1 items-center py-0"},a={class:"w-full"};function l(e,f){return t(),c("div",r,[o("div",a,[n(e.$slots,"default")])])}const i=s(_,[["render",l]]);export{i as _};
