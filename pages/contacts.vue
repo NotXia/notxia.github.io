34d8cf5b5c82f364704094196d0956be50ffbe13
NotXia <35894453+NotXia@users.noreply.github.com>
2025-04-09 20:58:41 +0200

    Update mail

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -9,7 +9,7 @@
         
         <div class="md:flex justify-center">
             <div>
-                <ContactLink label="tcxia@ik.me" url="mailto:tcxia@ik.me" :icon="mail_icon" alt="E-mail" />
+                <ContactLink label="tcxia [at] ik.me" :icon="mail_icon" alt="E-mail" />
                 <ContactLink label="t.me/notxia" url="https://t.me/notxia" :icon="telegram_icon" alt="Telegram" />
                 <ContactLink label="notpanda__" :icon="discord_icon" alt="Discord" />
                 <ContactLink label="linkedin.com/in/tian-cheng-xia" url="https://www.linkedin.com/in/tian-cheng-xia/" :icon="linkedin_icon" alt="LinkedIn" />
