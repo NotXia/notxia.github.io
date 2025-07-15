8afc8b740b7732421d45fb1d0f53a506d198aaca
NotXia <35894453+NotXia@users.noreply.github.com>
2025-07-15 21:42:48 +0900

    Add NII internship

--- components/resume/sections/Work.vue
+++ components/resume/sections/Work.vue
@@ -2,8 +2,18 @@
     <div>
         <h2 class="text-4xl font-bold tracking-wide">{{ $t("working experience") }}</h2>
 
-        <ActivityParagraph :title="$t('pcto toyota')" subtitle="Toyota Material Handling Manufacturing Italy" 
-                           :right_text="`${$t('december')} 2019 | ${$t('july')} 2019 | ${$t('february')} 2019`">
+        <ActivityParagraph 
+            :title="$t('research intern')" 
+            :subtitle="`National Institute of Informatics, Tokyo, ${$t('japan')}`" 
+            :right_text="`${$t('july')} 2025 - ${$t('january')} 2026`"
+        >
+        </ActivityParagraph>
+
+        <ActivityParagraph 
+            :title="$t('pcto toyota')" 
+            :subtitle="`Toyota Material Handling Manufacturing, Bologna, ${$t('italy')}`" 
+            :right_text="`${$t('december')} 2019 | ${$t('july')} 2019 | ${$t('february')} 2019`"
+        >
             {{ $t("pcto toyota description") }}
             <ul class="list-inside list-disc">
                 <li>{{ $t("pcto toyota description list1") }}</li>
