8e7f602443434a7ab22f289481d01ba030c823c4
NotXia <35894453+NotXia@users.noreply.github.com>
2025-11-19 08:51:02 +0900

    Update publications

---
+++ components/resume/paper-reference/JournalReference.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="md:ml-2 mt-4">
+        <span v-for="(author, i) in props.authors">
+            <span :class="`${props.highlight_author == author ? 'font-bold' : ''}`">{{ author }}</span><span v-if="i < props.authors.length-1">; </span>
+        </span>. 
+        ({{ props.year }}). "{{ props.title }}". 
+        <span class="italic">{{ journal }}</span>. 
+        <ReferenceLink :doi="doi" :openreview="openreview" />
+    </div>
+</template>
+
+
+<script setup lang="ts">
+    const props = defineProps({
+        title: String,
+        authors: { type: Array<String>, default: [] },
+        highlight_author: String,
+        year: String,
+        journal: String,
+        doi: { type: String, default: '' },
+        openreview: { type: String, default: '' }
+    });
+</script>
---
+++ components/resume/paper-reference/ReferenceLink.vue
@@ -0,0 +1,13 @@
+<template>
+    <span v-if="doi === '' && openreview === ''">{{ $t("under_review") }}</span>
+    <span v-if="doi !== ''"><span class="font-mono">DOI</span>: <a :href="`https://doi.org/${props.doi}`" class="font-mono hover:underline">{{ props.doi }}</a>.</span>
+    <span v-if="openreview !== ''"><span class="font-mono">OpenReview</span>: <a :href="`https://openreview.net/forum?id=${props.openreview}`" class="font-mono hover:underline">{{ props.openreview }}</a>.</span>
+</template>
+
+
+<script setup lang="ts">
+    const props = defineProps({
+        doi: { type: String, default: '' },
+        openreview: { type: String, default: '' }
+    });
+</script>
