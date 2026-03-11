b6d49f6d540b58beaa4df425019edb5fd44a9618
NotXia <35894453+NotXia@users.noreply.github.com>
2026-03-11 23:25:01 +0100

    Update publications

--- components/resume/paper-reference/ReferenceLink.vue
+++ components/resume/paper-reference/ReferenceLink.vue
@@ -1,13 +1,15 @@
 <template>
-    <span v-if="doi === '' && openreview === ''">{{ $t("under_review") }}</span>
+    <span v-if="doi === '' && openreview === '' && arxiv === ''">{{ $t("under_review") }}</span>
     <span v-if="doi !== ''"><span class="font-mono">DOI</span>: <a :href="`https://doi.org/${props.doi}`" class="font-mono hover:underline">{{ props.doi }}</a>.</span>
     <span v-if="openreview !== ''"><span class="font-mono">OpenReview</span>: <a :href="`https://openreview.net/forum?id=${props.openreview}`" class="font-mono hover:underline">{{ props.openreview }}</a>.</span>
+    <span v-if="arxiv !== ''"><span class="font-mono">arXiv</span>: <a :href="`https://arxiv.org/abs/${props.arxiv}`" class="font-mono hover:underline">{{ props.arxiv }}</a>.</span>
 </template>
 
 
 <script setup lang="ts">
     const props = defineProps({
         doi: { type: String, default: '' },
-        openreview: { type: String, default: '' }
+        openreview: { type: String, default: '' },
+        arxiv: { type: String, default: '' }
     });
 </script>
