f68260ac454d5a25ba90e08390e1a383e93b0fba
NotXia <35894453+NotXia@users.noreply.github.com>
2026-04-14 23:25:20 +0200

    Add dedicated publications section

--- components/resume/paper-reference/ConferenceReference.vue
+++ components/resume/paper-reference/ConferenceReference.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="md:ml-2 mt-4">
+        ({{ props.year }}). {{ props.title }}.<br/>
         <span v-for="(author, i) in props.authors">
             <span :class="`${props.highlight_author == author ? 'font-bold' : ''}`">{{ author }}</span><span v-if="i < props.authors.length-1">; </span>
-        </span>. 
-        ({{ props.year }}).<br/>
-        "{{ props.title }}".<br/>
+        </span>. <br/>
         <span>{{ conference }}</span>.<br/> 
         <ReferenceLink :doi="doi" :openreview="openreview" :arxiv="arxiv" />
     </div>
