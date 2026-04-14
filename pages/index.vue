50c8127fd45603f96ff2c776fe979e16a9390ed9
NotXia <35894453+NotXia@users.noreply.github.com>
2026-04-14 23:10:29 +0200

    Update home

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,12 @@
     <main>
         <div class="block md:flex justify-center items-center">
             <div class="text-center md:text-left">
-                <p class="text-2xl md:text-3xl font-semibold uppercase">{{ $t("i am a") }}</p>
+                <!-- <p class="text-2xl md:text-3xl font-semibold uppercase">{{ $t("i am a") }}</p>
                 <p class="text-4xl md:text-5xl font-semibold uppercase">{{ $t("cs student") }}</p>
-                <p class="text-xl lowercase">{{ $t("a very accurate description of me") }}</p>
+                <p class="text-xl lowercase">{{ $t("a very accurate description of me") }}</p> -->
+                <p class="text-2xl md:text-3xl font-semibold uppercase">{{ $t("i do things") }}</p>
+                <p class="text-2xl md:text-3xl font-semibold uppercase">{{ $t("with backpropagation") }}</p>
+                <p class="text-xl lowercase">{{ $t("that work most of the times") }}</p>
             </div>
 
             <div class="w-fit mx-auto mt-5 md:m-0 md:ml-10">
