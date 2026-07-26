91bb0dcaba919fcfbba486a4ffe5c61b5e3cb35b
NotXia <35894453+NotXia@users.noreply.github.com>
2026-07-26 17:37:45 +0200

    Update contacts

---
+++ pages/contacts.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="relative z-10">
+        <Navbar />
+    </div>
+
+    <ScreenCenter>
+    <main>
+        <!-- <h1 class="text-3xl md:text-5xl font-bold text-center mb-5">{{ t("contacts") }}</h1> -->
+        
+        <div class="md:flex justify-center">
+            <div>
+                <ContactLink label="riccardo.xia [at] outlook.com" :icon="mail_icon" alt="E-mail" />
+                <ContactLink label="t.me/notxia" url="https://t.me/notxia" :icon="telegram_icon" alt="Telegram" />
+                <ContactLink label="notpanda__" :icon="discord_icon" alt="Discord" />
+                <ContactLink label="linkedin.com/in/tian-cheng-xia" url="https://www.linkedin.com/in/tian-cheng-xia/" :icon="linkedin_icon" alt="LinkedIn" />
+                <ContactLink label="@notxia:matrix.notatoaster.ovh" url="https://matrix.to/#/@notxia:matrix.notatoaster.ovh" :icon="matrix_icon" alt="Matrix" />
+            </div>
+        </div>
+    </main>
+    </ScreenCenter>
+</template>
+
+
+<script setup lang="ts">
+    import mail_icon from "@/assets/images/icons/mail.svg";
+    import telegram_icon from "@/assets/images/icons/telegram.svg";
+    import linkedin_icon from "@/assets/images/icons/linkedin.svg";
+    import discord_icon from "@/assets/images/icons/discord.svg";
+    import matrix_icon from "@/assets/images/icons/matrix.svg";
+
+    const { t } = useI18n();
+    useHead({ title: t("title_contacts") });
+</script>
